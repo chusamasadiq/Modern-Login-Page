e08aefc23fabd318a91a0caadffa8f52354f8a14
Usama <Usamasadiq163@gmail.com>
2022-08-14 18:23:35 +0500

    Validatione Done

---
+++ script.js
@@ -0,0 +1,24 @@
+jQuery('#form').validate({
+	rules:{
+		email:{
+			required:true,
+			email:true
+		},
+		password:{
+			required:true,
+			minlength:8
+		},
+	},messages:{
+		email:{
+			required:"Please enter email",
+			email:"Please enter valid email",
+		},
+		password:{
+			required:"Please enter your password",
+			minlength:"Password must be 8 characters long"
+		},
+	},
+	submitHandler:function(form){
+		form.submit();
+	}
+});
